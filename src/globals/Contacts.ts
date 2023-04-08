6213a855651be243cbbbde468560f989b3f1b597
ignaskmn <ignas@kmn.lt>
2023-04-08 23:37:12 +0300

    Contact fields required

--- src/globals/Contacts.ts
+++ src/globals/Contacts.ts
@@ -14,25 +14,30 @@ const Contacts: GlobalConfig = {
         elements: ["link"],
         leaves: ["bold", "italic", "underline"],
       },
+      required: true,
     },
     {
       name: "phone",
       label: "Phone",
       type: "text",
+      required: true,
     },
     {
       name: "mphone",
       label: "Mobile Phone",
       type: "text",
+      required: true,
     },
     {
       name: "email",
       type: "text",
+      required: true,
     },
     {
       name: "background",
       type: "upload",
       relationTo: "images",
+      required: true,
     },
   ],
 };
